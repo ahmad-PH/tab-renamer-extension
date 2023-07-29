9e496f33f90c032ea5e5942f896eeb3882fad58e
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2023-07-28 22:44:49 -0400

    Add cmd+shift+y keyboard shortcut for triggering

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
       "contexts": ["all"]
     });
   });
-  
+
   chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "renameTab") {
       chrome.windows.create({
@@ -17,5 +17,18 @@ chrome.runtime.onInstalled.addListener(function() {
         // save tab id for later
         chrome.storage.sync.set({renamingTabId: tab.id});
       });
+    }
+  });
+
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log('command listener');
+    if(command === 'rename_tab') {
+        console.log(' it is a rename tab');
+        chrome.windows.create({
+        url: chrome.runtime.getURL("rename.html"),
+        type: "popup",
+        width: 300,
+        height: 200
+        });
     }
   });
--- rename.js
+++ rename.js
@@ -1,13 +1,28 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('rename').onclick = function() {
+document.addEventListener('DOMContentLoaded', function() {
+    let renameInput = document.getElementById('newName');
+  
+    // Autofocus on the text box
+    renameInput.focus();
+  
+    // Handle Enter key press
+    renameInput.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        // Get the tab ID from the query string
+        let url = new URL(window.location.href);
+        let tabId = parseInt(url.searchParams.get('tabId'));
+
         var newName = document.getElementById('newName').value;
         chrome.storage.sync.get(['renamingTabId'], function(result) {
             console.log('result from sync:', result);
-        renameTab(result.renamingTabId, newName);
+            renameTab(result.renamingTabId, newName);
         });
-    }
-});
   
+        // Rename the tab
+        // chrome.tabs.sendMessage(tabId, {name: renameInput.value});
+      }
+    });
+  });
+
 function renameTab(tabId, newName) {
 chrome.scripting.executeScript(
     {
