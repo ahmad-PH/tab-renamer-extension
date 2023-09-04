4924dbefe5366d7a8c176f8df09aa3737af463e7
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2023-09-04 18:58:57 -0400

    Create a basic emoji picker

--- contentScript.js
+++ contentScript.js
@@ -43,7 +43,6 @@ function setUIVisibility(visible) {
     }
 }
 
-
 chrome.runtime.onMessage.addListener(
     (message, sender, sendResponse) => {
         if (message.command === "open_rename_dialog") {
@@ -52,12 +51,17 @@ chrome.runtime.onMessage.addListener(
                     <div id="${rootElementId}">
                         <div id="${overlayId}"></div>
                         <div id="tab-renamer-extension-input-container">
-                            <div id="tab-renamer-extension-favicon-picker"></div>
+                            <div id="tab-renamer-extension-favicon-picker">
+                                <img id="emoji-picker-image"/>
+                                <div id="emoji-picker"> </div>
+                            </div>
                             <input type="text" id="${inputBoxId}" placeholder="New tab name" autocomplete="off" autofocus/>
                         </div>
                     </div>
                 `;
                 document.body.insertAdjacentHTML('beforeend', htmlContent);
+                document.getElementById("emoji-picker-image").src = chrome.runtime.getURL("emoji_picker_icon.png");
+                populateEmojiPicker("emoji-picker");
 
                 // Add Enter key listener to change the tab name
                 const inputBox = document.getElementById(inputBoxId);
@@ -133,3 +137,59 @@ function setEmojiFavicon(emoji) {
     // Set the favicon
     link.href = faviconURL;
 }
+
+
+async function populateEmojiPicker(emojiPickerId) {
+    let emojis = null;
+    try {
+        // TODO: Refactor this somewhere better that makes more sense:
+        // (Shouldn't be loaded every single time)
+        emojis = await (await fetch(chrome.runtime.getURL('emojis.json'))).json();
+    } catch (e) {
+        console.error('Error loading emojis:', e)
+    }
+
+    const emojiPickerElement = document.getElementById(emojiPickerId);
+
+    console.log('loaded emojis:', emojis);
+  
+    for (const category in emojis) {
+      // Create a div for each category
+      const categoryDiv = document.createElement('div');
+      categoryDiv.className = 'emoji-category';
+      categoryDiv.id = category;
+  
+      // You can also add a title for each category
+      const categoryTitle = document.createElement('div');
+      categoryTitle.className = 'emoji-category-title';
+      categoryTitle.innerText = category.replace(/_/g, ' '); // replace underscores with spaces
+      categoryDiv.appendChild(categoryTitle);
+        
+      // Create emoji elements
+      const categoryEmojis = document.createElement('div'); 
+      categoryEmojis.className = 'emoji-category-emojis';
+      for (const emoji of emojis[category]) {
+        const emojiElement = document.createElement('span');
+        emojiElement.className = 'emoji-item';
+        emojiElement.dataset.unicode = emoji.unicode;
+        emojiElement.dataset.shortcode = emoji.shortcode;
+        
+        // Display the emoji. Assuming "unicode" in JSON is in the format "U+1F600"
+        emojiElement.textContent = String.fromCodePoint(parseInt(emoji.unicode.replace("U+", ""), 16));
+        emojiElement.addEventListener('click', function() {
+          // You can define what happens when an emoji is clicked here
+          // For example: setEmojiFavicon(this.dataset.unicode);
+        });
+  
+        // Append the emoji element to the category div
+        categoryEmojis.appendChild(emojiElement);
+      }
+      categoryDiv.appendChild(categoryEmojis);
+  
+      // Append the category div to the emoji picker
+      emojiPickerElement.appendChild(categoryDiv);
+    }
+  }
+  
+
+  
