067bf05cdf0b2e9cdb1ed3c163914f916436fde0
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2024-02-05 22:45:23 -0500

    Refactor EmojiPicker component to use modern React practices
    (functional component + hooks)

--- src/config.js
+++ src/config.js
@@ -1,12 +1,21 @@
 const EXTENSION_PREFIX = "tab-renamer-extension";
+
+// App:
 export const ROOT_ELEMENT_ID = `${EXTENSION_PREFIX}-root`;
 export const ROOT_TAG_NAME = `tab-renamer-root`
 export const INPUT_BOX_ID = `${EXTENSION_PREFIX}-input-box`;
 export const OVERLAY_ID = `${EXTENSION_PREFIX}-overlay`;
+
+// Selected Emoji:
 export const FAVICON_PICKER_ID = `${EXTENSION_PREFIX}-favicon-picker`;
+
+// Emoji Picker:
 export const EMOJI_PICKER_ID = `${EXTENSION_PREFIX}-emoji-picker`;
 export const EMOJI_PICKER_IMAGE_ID = `${EXTENSION_PREFIX}-emoji-picker-image`;
+export const SEARCH_BAR_ID = 'tab-renamer-extension-emoji-search-bar';
+export const SEARCH_RESULTS_ID = 'tab-renamer-extension-search-results-div';
 export const PICKED_EMOJI_ID = `${EXTENSION_PREFIX}-picked-emoji`;
 export const EMOJI_REMOVE_BUTTON_ID = `${EXTENSION_PREFIX}-emoji-remove-button`;
+export const ALL_EMOJIS_ID = 'tab-renamer-extension-all-emojis-div';
 
 export const EVENT_OPEN_RENAME_DIALOG = `open_rename_dialog`;
--- src/contentScript/components/EmojiPicker/EmojiPicker.js
+++ src/contentScript/components/EmojiPicker/EmojiPicker.js
@@ -1,99 +1,88 @@
-import React, { Component, useEffect, useRef } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styles from './EmojiPicker.module.css';
 import PropTypes from 'prop-types';
-import { EMOJI_PICKER_ID, EMOJI_REMOVE_BUTTON_ID } from '../../../config';
+import { EMOJI_PICKER_ID, EMOJI_REMOVE_BUTTON_ID, SEARCH_BAR_ID, SEARCH_RESULTS_ID, ALL_EMOJIS_ID } from '../../../config';
 import classNames from 'classnames';
 
-const SEARCH_RESULTS_ID = 'tab-renamer-extension-search-results-div';
-const ALL_EMOJIS_ID = 'tab-renamer-extension-all-emojis-div';
-const SEARCH_BAR_ID = 'tab-renamer-extension-emoji-search-bar';
 
-/**
- * EmojiPicker component.
- *
- * @component
- * @param {Object} props
- * @param {Function} props.onEmojiClick - Function to call when an emoji is clicked.
- * @returns {React.Component} EmojiPicker component.
- */
-class EmojiPicker extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchValue: '',
-            emojis: [],
-            allEmojis: null,
-        };
-    }
-
-    async componentDidMount() {
-        try {
-            this.setState({allEmojis: await (await fetch(chrome.runtime.getURL('assets/emojis.json'))).json()});
-        } catch (e) {
-            console.error('Error loading emojis:', e);
-        }
-    }
-
-    filterEmojis(searchValue) {
+const EmojiPicker = ({ onEmojiClick, onRemoveEmoji, isVisible }) => {
+    const [searchValue, setSearchValue] = useState('');
+    const [allEmojis, setAllEmojis] = useState({});
+    
+    const findMatchingEmojis = (searchValue, emojis) => {
         let filteredEmojis = [];
-        for (const category in this.state.emojis) {
-            filteredEmojis = filteredEmojis.concat(this.state.emojis[category].filter(emoji =>
+        for (const category in emojis) {
+            filteredEmojis = filteredEmojis.concat(emojis[category].filter(emoji =>
                 emoji.shortcode.includes(searchValue)
             ));
         }
         return filteredEmojis;
     }
 
-    formatEmojiCategoryTitle(categoryTitle) {
+    const matchingEmojis = useMemo(
+        () => findMatchingEmojis(searchValue, allEmojis),
+        [searchValue, allEmojis]
+    );
+
+    useEffect(() => {
+        const loadEmojis = async () => {
+            try {
+                const response = await fetch(chrome.runtime.getURL('assets/emojis.json'));
+                setAllEmojis(await response.json());
+            } catch (e) {
+                console.error('Error loading emojis:', e);
+            }
+        };
+
+        loadEmojis();
+    }, []);
+
+    const formatEmojiCategoryTitle = (categoryTitle) => {
         return categoryTitle
             .replace(/_/g, ' ')  // Replace underscores with spaces
             .replace(/\band\b/gi, '&')  // Replace 'and' with '&'
             .replace(/\b\w/g, (char) => char.toUpperCase());  // Capitalize first letter of each word
-    }
+    };
 
-    render() {
-        return (
-            <div id={EMOJI_PICKER_ID} className={classNames(styles.root, {[styles.visible]: this.props.isVisible})}>
-                <div className={styles.headerContainer}>
-                    <div className={styles.header}>
-                        <SearchBar onSearchBarChanged={(searchValue) => this.setState({searchValue})} />
-                        <button id={EMOJI_REMOVE_BUTTON_ID} onClick={this.props.onRemoveEmoji} className={styles.removeButton}>
-                            Remove
-                        </button>
-                    </div>
-                </div>
-
-                <div className={styles.content}>
-                    {this.state.searchValue === "" ? (
-                        <div id={ALL_EMOJIS_ID}>
-                            {this.state.allEmojis && Object.entries(this.state.allEmojis).map(([category, emojis]) => (
-                                <div key={category}>
-                                    <div className={styles.emojiCategoryTitle}>
-                                        {this.formatEmojiCategoryTitle(category)}
-                                    </div>
-                                    <div className={styles.emojiGrid}>
-                                        {emojis.map(emoji => (
-                                            <Emoji emoji={emoji} key={emoji.unicode} onClick={
-                                                () => this.props.onEmojiClick(emoji.emoji)
-                                            }/>
-                                        ))}
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <div id={SEARCH_RESULTS_ID} className={classNames(styles.searchResults, styles.emojiGrid)}>
-                            {this.state.emojis.map(emoji => (
-                                <Emoji emoji={emoji} key={emoji.unicode} onClick={
-                                    this.props.onEmojiClick
-                                }/>
-                            ))}
-                        </div>
-                    )}
+    return (
+        <div id={EMOJI_PICKER_ID} className={classNames(styles.root, {[styles.visible]: isVisible})}>
+            <div className={styles.headerContainer}>
+                <div className={styles.header}>
+                    <SearchBar onSearchBarChanged={(searchValue) => setSearchValue(searchValue)} />
+                    <button id={EMOJI_REMOVE_BUTTON_ID} onClick={onRemoveEmoji} className={styles.removeButton}>
+                        Remove
+                    </button>
                 </div>
             </div>
-        );
-    }
+
+            <div className={styles.content}>
+                {searchValue === "" ? (
+                    <div id={ALL_EMOJIS_ID}>
+                        {allEmojis && Object.entries(allEmojis).map(([category, emojis]) => (
+                            <div key={category}>
+                                <div className={styles.emojiCategoryTitle}>
+                                    {formatEmojiCategoryTitle(category)}
+                                </div>
+                                <div className={styles.emojiGrid}>
+                                    {emojis.map(emoji => (
+                                        <Emoji emoji={emoji} key={emoji.unicode} onClick={
+                                            () => onEmojiClick(emoji.emoji)
+                                        }/>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div id={SEARCH_RESULTS_ID} className={classNames(styles.searchResults, styles.emojiGrid)}>
+                        {matchingEmojis.map(emoji => (
+                            <Emoji emoji={emoji} key={emoji.unicode} onClick={onEmojiClick}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 EmojiPicker.propTypes = {
