971692f30bf16e311dc04892540a609039f5ffb2
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2024-06-09 19:40:18 -0400

    Add some logs in handleChromeUpdate

--- src/background/handleChromeUpdate.js
+++ src/background/handleChromeUpdate.js
@@ -1,13 +1,19 @@
 import { storageGet, storageSet } from '../utils.js';
+import { getLogger } from '../log.js';
+
+const log = getLogger('handleChromeUpdate', 'debug');
 
 async function handleChromeUpdate() {
+    log.debug('handleChromeUpdate called ...');
     const storedTabs = await storageGet(null);
+    log.debug('current tabs:', storedTabs);
     for (const tabId in storedTabs) {
         if (!storedTabs[tabId].isClosed) {
             storedTabs[tabId].isClosed = true;
             storedTabs[tabId].closedAt = new Date().toISOString();
         }
     }
+    log.debug('new tabs:', storedTabs);
     await storageSet(storedTabs);
 }
 
