b373d01c915410f97bef4ec3168fc99f0f287e5d
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2025-04-06 11:53:51 -0400

    Fix the issue with ESC and click-out not canceling the overlay

--- src/contentScript/components/App/App.js
+++ src/contentScript/components/App/App.js
@@ -8,7 +8,7 @@ import FaviconPicker from '../FaviconPicker';
 import { TabSignature } from '../../../types';
 import { Favicon, SystemEmojiFavicon, TwemojiFavicon, UrlFavicon } from '../../../favicon';
 import SettingsButton from '../SettingsButton';
-import Frame from 'react-frame-component';
+import Frame, { FrameContextConsumer } from 'react-frame-component';
 
 /* Non-functional note: Any slow-down in this file, for example in the imports above, will lead to a slower execution time 
  * for the insertUIIntoDOM function, because contentScript.js will import App only when this function is called. Adding an
@@ -45,21 +45,6 @@ export default function App() {
             }
         }, [isVisible]);
     }
-
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            if (event.key === "Escape") {
-                setIsVisible(false);
-            }
-        };
-
-        document.addEventListener("keydown", handleKeyDown);
-
-        return () => {
-            document.removeEventListener("keydown", handleKeyDown);
-        };
-    }, []);
-
     useStopKeyEventsPropagation(isVisible, ['Escape', 'Enter']);
 
     useEffect(() => {
@@ -153,39 +138,53 @@ export default function App() {
     
     return (
         <div id={ROOT_ELEMENT_ID} className={styles.root} style={{ display: isVisible ? 'block' : 'none' }}>
-            <div id={OVERLAY_ID} className={styles.overlay} onClick={() => {setIsVisible(false)}}/>
             <Frame
                 head={cloneShadowRootStyles()} 
                 className = {styles.mainBarIFrameContainer}
                 initialContent={`<!DOCTYPE html><html class="${styles.inputBoxHTML}"><head></head><body class="${styles.inputBoxBody}" id="mountTarget"></body></html>`}
                 mountTarget="#mountTarget"
             >
-                <div className={styles.mainBarContainer}>
-                    <div className={styles.mainBar}>
-                        <div className={styles.faviconPickerWrapper}>
-                            <SelectedFavicon selectedFavicon={selectedFavicon} handleSelectedFaviconClick={handleSelectedFaviconClick}/>
-                            {emojiPickerIsVisible && 
-                                <FaviconPicker 
-                                    onFaviconClick={handleFaviconClick}
-                                    onRemoveEmoji={handleRemoveEmoji}
-                                />
+                <FrameContextConsumer>
+                    {({document}) => {
+                        document.addEventListener('keydown', (event) => {
+                            if (event.key === "Escape") {
+                                setIsVisible(false);
                             }
-                        </div>
-
-                        <input
-                            type="text"
-                            id={INPUT_BOX_ID}
-                            className={styles.inputBox}
-                            placeholder="New Tab Title"
-                            autoComplete="off"
-                            value={inputBoxValue}
-                            onChange={(event) => setInputBoxValue(event.target.value)}
-                            onClick={(event) => event.stopPropagation()}
-                            onKeyDown={handleInputBoxKeydown}
-                            ref={inputRef}
-                        />
-                    </div>
-                </div>
+                        });
+                        return (
+                            <>
+                                <div id={OVERLAY_ID} className={styles.overlay} onClick={() => {setIsVisible(false)}}/>
+                                <div className={styles.mainBarContainer}>
+                                    <div className={styles.mainBar}>
+                                        <div className={styles.faviconPickerWrapper}>
+                                            <SelectedFavicon selectedFavicon={selectedFavicon} handleSelectedFaviconClick={handleSelectedFaviconClick}/>
+                                            {emojiPickerIsVisible && 
+                                                <FaviconPicker 
+                                                    onFaviconClick={handleFaviconClick}
+                                                    onRemoveEmoji={handleRemoveEmoji}
+                                                />
+                                            }
+                                        </div>
+            
+                                        <input
+                                            type="text"
+                                            id={INPUT_BOX_ID}
+                                            className={styles.inputBox}
+                                            placeholder="New Tab Title"
+                                            autoComplete="off"
+                                            value={inputBoxValue}
+                                            onChange={(event) => setInputBoxValue(event.target.value)}
+                                            onClick={(event) => event.stopPropagation()}
+                                            onKeyDown={handleInputBoxKeydown}
+                                            ref={inputRef}
+                                        />
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }}
+                    
+                </FrameContextConsumer>
             </Frame>
             <SettingsButton />
         </div>
