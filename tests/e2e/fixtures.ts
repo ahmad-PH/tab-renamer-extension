d48e3abcf31efc9925f406189264fa7f4dd3f866
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2025-09-14 15:31:01 -0400

    Make the extension actually load

---
+++ tests/e2e/fixtures.ts
@@ -0,0 +1,32 @@
+import { test as base, chromium, type BrowserContext } from '@playwright/test';
+import path from 'path';
+import appRootPath from 'app-root-path';
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+}>({
+  context: async ({ }, use) => {
+    console.log('Project root:', appRootPath.path);
+    const pathToExtension = path.join(appRootPath.path, 'dist/dev');
+    const context = await chromium.launchPersistentContext('', {
+      channel: 'chromium',
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    // for manifest v3:
+    let [serviceWorker] = context.serviceWorkers();
+    if (!serviceWorker)
+      serviceWorker = await context.waitForEvent('serviceworker');
+
+    const extensionId = serviceWorker.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+export const expect = test.expect;
