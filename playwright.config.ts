d5e9b14b7968186bdee70da7a632cfa07234e6cd
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2025-11-23 11:54:01 -0500

    Some cleanups

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
+const baseURL = 'http://localhost:3000';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -20,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -70,8 +71,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'node tests/e2e/server.js',
-    url: 'http://localhost:3000',
-    reuseExistingServer: false,
+    url: baseURL,
+    reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
     stdout: 'pipe',
     stderr: 'pipe',
