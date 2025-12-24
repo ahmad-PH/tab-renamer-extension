2eabdf4525f49b851cee46c04f04f01bcf0bf693
Ahmad Pourihosseini <seyedahmadpourihosseini@gmail.com>
2025-12-23 19:12:33 -0500

    Improve markAllOpenSignaturesAsClosed

--- src/background/markAllOpenSignaturesAsClosed.ts
+++ src/background/markAllOpenSignaturesAsClosed.ts
@@ -4,16 +4,15 @@ import { getLogger } from '../log';
 const log = getLogger('markAllOpenSignaturesAsClosed');
 
 async function markAllOpenSignaturesAsClosed(): Promise<void> {
-    log.debug('markAllOpenSignaturesAsClosed called ...');
     const storedTabs = await tabRepository.getAll();
-    log.debug('current tabs:', storedTabs);
+    log.debug(`markAllOpenSignaturesAsClosed called, with the current collection of tabs: ${JSON.stringify(storedTabs)}`);
     for (const tab of storedTabs) {
         if (!tab.isClosed) {
             tab.isClosed = true;
             tab.closedAt = new Date().toISOString();
         }
     }
-    log.debug('new tabs:', storedTabs);
+    log.debug('markAllOpenSignaturesAsClosed updated tabs:', JSON.stringify(storedTabs));
     await tabRepository.updateMany(storedTabs);
 }
 
